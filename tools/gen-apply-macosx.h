574f0d52769961fb7bdce780086861807bd9ea74
Rhys Weatherley <rweather@southern-storm.com.au>
2004-06-07 07:15:30 +0000

    Bypass the auto-detection logic in gen-apply under MacOSX because it doesn't work with Apple's version of gcc.

---
+++ tools/gen-apply-macosx.h
@@ -0,0 +1,67 @@
+/*
+ * gen-apply-macosx.h - MacOSX-specific definitions.
+ *
+ * Copyright (C) 2004  Southern Storm Software, Pty Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef	_GEN_APPLY_MACOSX_H
+#define	_GEN_APPLY_MACOSX_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define JIT_APPLY_NUM_WORD_REGS 8
+#define JIT_APPLY_NUM_FLOAT_REGS 13
+#define JIT_APPLY_PASS_STACK_FLOAT_AS_DOUBLE 0
+#define JIT_APPLY_PASS_STACK_FLOAT_AS_NFLOAT 0
+#define JIT_APPLY_PASS_STACK_DOUBLE_AS_NFLOAT 0
+#define JIT_APPLY_PASS_STACK_NFLOAT_AS_DOUBLE 1
+#define JIT_APPLY_PASS_REG_FLOAT_AS_DOUBLE 1
+#define JIT_APPLY_PASS_REG_FLOAT_AS_NFLOAT 0
+#define JIT_APPLY_PASS_REG_DOUBLE_AS_NFLOAT 0
+#define JIT_APPLY_PASS_REG_NFLOAT_AS_DOUBLE 1
+#define JIT_APPLY_RETURN_FLOAT_AS_DOUBLE 1
+#define JIT_APPLY_RETURN_FLOAT_AS_NFLOAT 0
+#define JIT_APPLY_RETURN_DOUBLE_AS_NFLOAT 0
+#define JIT_APPLY_RETURN_NFLOAT_AS_DOUBLE 1
+#define JIT_APPLY_FLOATS_IN_WORD_REGS 0
+#define JIT_APPLY_RETURN_FLOATS_AFTER 8
+#define JIT_APPLY_VARARGS_ON_STACK 0
+#define JIT_APPLY_STRUCT_RETURN_SPECIAL_REG 0
+#define JIT_APPLY_STRUCT_REG_OVERLAPS_WORD_REG 0
+#define JIT_APPLY_ALIGN_LONG_REGS 1
+#define JIT_APPLY_ALIGN_LONG_STACK 1
+#define JIT_APPLY_CAN_SPLIT_LONG 0
+#define JIT_APPLY_STRUCT_RETURN_IN_REG \
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+#define JIT_APPLY_MAX_STRUCT_IN_REG 0
+#define JIT_APPLY_X86_FASTCALL 0
+#define JIT_APPLY_PARENT_FRAME_OFFSET 0
+#define JIT_APPLY_RETURN_ADDRESS_OFFSET 0
+#define JIT_APPLY_BROKEN_FRAME_BUILTINS 1
+#define JIT_APPLY_X86_POP_STRUCT_RETURN 0
+#define JIT_APPLY_PAD_FLOAT_REGS 1
+
+#ifdef	__cplusplus
+};
+#endif
+
+#endif	/* _GEN_APPLY_MACOSX_H */
